05c9154434e3d1f93e892912107012f05db88b44
Rishabh verma <25744589+rishabhv471@users.noreply.github.com>
2023-10-23 15:11:12 +0530

    Update RestaurantMenu.jsx

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React from 'react'
 import { useParams } from 'react-router-dom';
 import { SwiggyMenuAPi } from '../utils/env';
 import { additem } from '../utils/cartSlice';
