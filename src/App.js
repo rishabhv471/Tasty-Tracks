447d41d21e024518a1c24c59b827feaeb4229bd7
Rishabh verma <25744589+rishabhv471@users.noreply.github.com>
2023-10-23 15:03:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
 import store from "./utils/store";
 import Footer from "./Components/Footer";
-
+import React, { Component }  from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
  
 
