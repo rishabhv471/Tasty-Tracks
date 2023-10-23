650d31807d3054525225c4cc91ae0ba40f77b85a
Rishabh verma <25744589+rishabhv471@users.noreply.github.com>
2023-10-23 14:54:21 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import React from 'react';
 import '../App.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse, faAddressCard, faCartPlus } from "@fortawesome/free-solid-svg-icons";
